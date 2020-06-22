ada712abbf51e2db475b13779a6fc088a33839e1
Juan Pablo Bustos Paulich <juanpaulich2020@gmail.com>
2020-06-21 22:27:43 -0300

    Initial Commit

---
+++ src/main/java/com/shyaragas/app/ShyaraGasApplication.java
@@ -0,0 +1,13 @@
+package com.shyaragas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShyaraGasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShyaraGasApplication.class, args);
+	}
+
+}
